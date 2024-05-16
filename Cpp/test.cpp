da39e36c7294b11a1db849c2153bb834d104cbd0
shaunchen284 <shaunchen284@gmail.com>
2024-05-16 10:05:32 +0800

    516

---
+++ Cpp/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lenovo on 2024/5/16.
+//
+
+#include "test.h"
---
+++ Cpp/test.h
@@ -0,0 +1,16 @@
+//
+// Created by lenovo on 2024/5/16.
+//
+
+#ifndef CPP_TEST_H
+#define CPP_TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //CPP_TEST_H
