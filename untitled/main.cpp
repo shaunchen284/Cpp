86c83aa21426ee3f0f546911d4db771e255911f2
shaunchen284 <shaunchen284@gmail.com>
2024-05-16 10:00:06 +0800

    123

---
+++ untitled/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+# define day 7
+using namespace std;
+int main() {
+//    cout << "Hello, World!" << " ";
+    int a = 10;
+    cout<<'day '<<a<<endl;
+
+    system("pause");
+    return 0;
+    //123
+    /*
+     *
+     */
+
+
+}
